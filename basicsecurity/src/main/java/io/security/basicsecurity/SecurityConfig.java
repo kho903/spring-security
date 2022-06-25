fd083b70415efa862cf45f7c02b84f9c7def1c84
kho903 <gmldnr2222@naver.com>
2022-06-25 14:41:50 +0900

    Spring Security / 01. 스프링 시큐리티 기본 API 및 Filter 이해 / 03. Form Login 인증

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,10 +1,23 @@
 package io.security.basicsecurity;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,10 +25,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http // 인가정책
-				.authorizeRequests()
-				.anyRequest().authenticated();
+			.authorizeRequests()
+			.anyRequest().authenticated();
 
 		http // 인증정책
-				.formLogin();
+			.formLogin()
+			//.loginPage("/loginPage")
+			.defaultSuccessUrl("/")
+			.failureUrl("/login")
+			.usernameParameter("userId")
+			.passwordParameter("passwd")
+			.loginProcessingUrl("/login_proc")
+			.successHandler(new AuthenticationSuccessHandler() {
+				@Override
+				public void onAuthenticationSuccess(
+					HttpServletRequest request,
+					HttpServletResponse response, Authentication authentication
+				) throws IOException, ServletException {
+					log.info("authentication={}", authentication.getName());
+					response.sendRedirect("/");
+				}
+			})
+			.failureHandler(new AuthenticationFailureHandler() {
+				@Override
+				public void onAuthenticationFailure(
+					HttpServletRequest request,
+					HttpServletResponse response,
+					AuthenticationException exception
+				) throws IOException, ServletException {
+					log.info("exception={}", exception.getMessage());
+					response.sendRedirect("/login");
+				}
+			})
+			.permitAll()
+		;
 	}
 }
